6d281c5eb3c37747f8c66bb96f4f01f60b1d70b7
tharun571 <atharun05@gmail.com>
2020-09-26 13:33:54 +0530

    Dataset for inital model

--- mybot.py
+++ mybot.py
@@ -1,4 +1,5 @@
-import tweepy
+import tweepy, time, datetime
+import numpy as np
 
 CONSUMER_KEY = 'mqT85KoatMgF27uILbYGSpV2i'
 CONSUMER_SECRET = 'YKiBx5HNXjdg2dpfeShAQ69wncYlKhqtyjT6v7VtlXFbj3kZkr'
@@ -9,18 +10,47 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#getting the info of the tagged tweet
-metadata=api.mentions_timeline()
+#a and b are used to make sure the already predicted tweets are not replied again
 
-#getting the tweet id and user id of the original tweet
-tweetid=metadata[0].in_reply_to_status_id
-userid=metadata[0].in_reply_to_user_id
 
-#getting the status of the original tweet
-status = api.get_status(tweetid)
+def likes_predictor(tweetid,userid):
 
-#getting the status of the user
-user=api.get_user(userid)
+    no_of_likes = np.array([])
+    seconds_since_posted = np.array([])
+    no_of_followers = np.array([])
 
-print(status.__dict__.keys())
-print(user.__dict__.keys())
+    #getting the status of the original tweet
+    status = api.get_status(tweetid)
+
+    #getting the status of the user
+    user = api.get_user(userid)
+    d = status.created_at
+    age = time.time() - (d - datetime.datetime(1970,1,1)).total_seconds()
+
+    no_of_likes = np.append(no_of_likes , status.favorite_count)
+    seconds_since_posted = np.append(seconds_since_posted,age)
+    no_of_followers = np.append(no_of_followers,user.followers_count)
+    #print(no_of_likes)
+    #print(no_of_followers)
+    # print(seconds_since_posted)
+
+def reply_to_tweets(a):
+    b=1
+    #getting the info of the tagged tweet
+    mentions = api.mentions_timeline()
+    for mention in reversed(mentions):
+        if b>a:
+            a+=1
+            #getting the tweet id and user id of the original tweet
+            tweetid=mention.in_reply_to_status_id
+            userid=mention.in_reply_to_user_id
+            likes_predictor(tweetid,userid)
+            #api.update_status('@' + mention.user.screen_name +
+            #            'Hi', mention.id)
+        b+=1 
+
+# while True:
+#     reply_to_tweets()
+#     time.sleep(15)
+
+reply_to_tweets(0)
