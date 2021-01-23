567b9de784a445f3100b67e8ae258f381c3b19a0
Poobalan <pooblan12100@gmail.com>
2021-01-23 22:12:28 +0530

    modified

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# it is used keep track of the replied tweets
 tweet_id_list=[]
 
 def predict_likes(followers):
@@ -33,17 +34,29 @@ def reply_to_the_tweet(tweetid, likes_count):
         print("already predicted")
 
 def start():
+
     for mention in tweepy.Cursor(api.mentions_timeline).items():
+
+        # getting the mentioned tweet
+
         json_str=json.dumps(mention._json)
         parsed = json.loads(json_str)
+
+        # Below code is to get the followers and user information of the original tweet using the tweet id
         user=api.get_user(parsed["entities"]["user_mentions"][0]["id"])
         json_str1=json.dumps(user._json)
         parsed1=json.loads(json_str1)
+
+        #parsed1 is the user info object
+
         val=predict_likes(parsed1["followers_count"])
         reply_to_the_tweet(parsed["id"],val)
+        
+        # sleeping in order to avoid the rate limits
         time.sleep(60)
 
 
+# using schedule for running the code every one hour
 
 schedule.every(1).hour.do(start)
 while True:
